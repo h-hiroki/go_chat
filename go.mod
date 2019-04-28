eabd06171075a00876b8cf5e658f5ee2c6425cdb
h-hiroki <gyouzakuuka@gmail.com>
2019-04-28 20:07:44 +0900

    ⛳️HTTP2を受け付ける

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/h-hiroki/go_chat
 
 go 1.12
+
+require golang.org/x/net v0.0.0-20190424112056-4829fb13d2c6 // indirect
--- server.go
+++ server.go
@@ -2,35 +2,22 @@ package main
 
 import (
 	"fmt"
+	"golang.org/x/net/http2"
 	"net/http"
 )
 
-type HelloHandler struct {}
+type MyHandler struct{}
 
-func (h HelloHandler) ServeHTTP (w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello!")
-}
-
-func log(h http.Handler) http.Handler {
-	return http.HandlerFunc (func(w http.ResponseWriter, r *http.Request) {
-		fmt.Printf("Handler called - %T\n", h)
-		h.ServeHTTP(w, r)
-	})
-}
-
-func protect(h http.Handler) http.Handler {
-	return http.HandlerFunc(func (w http.ResponseWriter, r *http.Request) {
-		// ここにもなにか書けますよ
-		h.ServeHTTP(w, r)
-	})
+func (h *MyHandler) ServeHTTP (w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello World!")
 }
 
 func main() {
+	handler := MyHandler{}
 	server := http.Server{
 		Addr: "127.0.0.1:8080",
+		Handler: &handler,
 	}
-	hello := HelloHandler{}
-
-	http.Handle("/hello", protect(log(hello)))
-	server.ListenAndServe()
+	http2.ConfigureServer(&server, &http2.Server{})
+	server.ListenAndServeTLS("cert.pem", "key.pem")
 }
