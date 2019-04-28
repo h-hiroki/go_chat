b3b5a1821ccbcc112fddb143509eb82e50c44142
h-hiroki <gyouzakuuka@gmail.com>
2019-04-29 02:25:54 +0900

    ⛳️send cookies

--- server.go
+++ server.go
@@ -1,51 +1,26 @@
 package main
 
-import (
-	"encoding/json"
-	"fmt"
-	"net/http"
-)
+import "net/http"
 
-type Post struct {
-	User string
-	Threads []string
-}
-
-func writeExample(w http.ResponseWriter, r *http.Request) {
-	str := `<html>
-<head><title>Go Web Programming</title></head>
-<body><h1>Hello World</h1></body>
-</html>`
-	w.Write([]byte(str))
-}
-
-func writeHeaderExample(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(501)
-	fmt.Fprintln(w, "そんなの無いよ")
-}
-
-func headerExample(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Location", "https://google.com")
-	w.WriteHeader(302)
-}
-
-func jsonExample(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	post := &Post{
-		User: "hhiroki",
-		Threads: []string{"no1", "no2", "no3"},
+func setCookie(w http.ResponseWriter, r *http.Request) {
+	c1 := http.Cookie{
+		Name: "first_cookie",
+		Value: "Go Web Programming",
+		HttpOnly: true,
 	}
-	json, _ := json.Marshal(post)
-	w.Write(json)
+	c2 := http.Cookie{
+		Name: "second_cookie",
+		Value: "syakai",
+		HttpOnly: true,
+	}
+	w.Header().Set("Set-Cookie", c1.String())
+	w.Header().Add("Set-Cookie", c2.String())
 }
 
 func main() {
-	server := http.Server {
+	server := http.Server{
 		Addr: "127.0.0.1:8080",
 	}
-	http.HandleFunc("/write", writeExample)
-	http.HandleFunc("/writeheader", writeHeaderExample)
-	http.HandleFunc("/redirect", headerExample)
-	http.HandleFunc("/json", jsonExample)
+	http.HandleFunc("/set_cookie", setCookie)
 	server.ListenAndServe()
 }
