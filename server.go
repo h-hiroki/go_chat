7e630940fce4c5f4b6b61d154f4e4e1ba824f54c
h-hiroki <gyouzakuuka@gmail.com>
2019-04-29 01:40:31 +0900

    ⛳️change response header

--- server.go
+++ server.go
@@ -2,26 +2,27 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
 	"net/http"
 )
 
-func process(w http.ResponseWriter, r *http.Request) {
-	r.ParseMultipartForm(1024)
-	fileHeader := r.MultipartForm.File["uploaded"][0]
-	file, err := fileHeader.Open()
-	if err == nil {
-		data, err := ioutil.ReadAll(file)
-		if err == nil {
-			fmt.Fprintln(w, string(data))
-		}
-	}
+func writeExample(w http.ResponseWriter, r *http.Request) {
+	str := `<html>
+<head><title>Go Web Programming</title></head>
+<body><h1>Hello World</h1></body>
+</html>`
+	w.Write([]byte(str))
+}
+
+func writeHeaderExample(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(501)
+	fmt.Fprintln(w, "そんなの無いよ")
 }
 
 func main() {
 	server := http.Server {
 		Addr: "127.0.0.1:8080",
 	}
-	http.HandleFunc("/process", process)
+	http.HandleFunc("/write", writeExample)
+	http.HandleFunc("/writeheader", writeHeaderExample)
 	server.ListenAndServe()
 }
